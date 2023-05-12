6d74c230aa7bd2a74fadd2469ec8629819d70df9
Kottonboru <Aehryn_Personal@outlook.com>
2023-05-12 07:07:12 -0500

    Added Comments

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const { Client, Intents, ApplicationCommandOptionType } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
 
+// Create a new instance of the Discord client
 const client = new Client({
   intents: [
-    Intents.FLAGS.Guilds,
-    Intents.FLAGS.GuildMessages,
-    Intents.FLAGS.MessageContent,
-    Intents.FLAGS.GuildMembers,
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.MESSAGE_CONTENT,
+    Intents.FLAGS.GUILD_MEMBERS,
   ],
 });
 
+// Create a new instance of the OpenAI configuration
 const configuration = new Configuration({
   apiKey: process.env.API_KEY || '',
 });
 const openai = new OpenAIApi(configuration);
 
+// Create a set to store user IDs for enabling/disabling AI
 const aiEnabledUsers = new Set();
+
+// Maximum length for AI-generated messages
 const MAX_MESSAGE_LENGTH = 2000;
 
+// Event: Bot is ready and connected to Discord
 client.on('ready', async () => {
   console.log('The bot is online!!!');
 
   try {
+    // Get the guild ID from environment variables
     const guildId = process.env.GUILD_ID || '';
+
+    // Register slash commands for the guild
     const commands = await client.guilds.cache.get(guildId)?.commands.set([
       {
         name: 'noai',
@@ -47,6 +56,7 @@ client.on('ready', async () => {
         ],
       },
     ]);
+
     console.log('Registered slash commands:');
     console.log(commands);
   } catch (error) {
@@ -54,15 +64,20 @@ client.on('ready', async () => {
   }
 });
 
+// Event: User interaction with slash commands
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
   if (interaction.commandName === 'noai') {
+    // Get the toggle option from the interaction
     const toggle = interaction.options.getString('toggle')?.toLowerCase();
+
     if (toggle === 'on') {
+      // Disable AI for the user
       aiEnabledUsers.delete(interaction.user.id);
       await interaction.reply('AI is now disabled for your messages.');
     } else if (toggle === 'off') {
+      // Enable AI for the user
       aiEnabledUsers.add(interaction.user.id);
       await interaction.reply('AI is now enabled for your messages.');
     } else {
@@ -71,19 +86,23 @@ client.on('interactionCreate', async (interaction) => {
   }
 });
 
+// Event: User message in a designated channel
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
   if (message.channel.id !== process.env.CHANNEL_ID) return;
 
   if (aiEnabledUsers.has(message.author.id)) {
+    // Create a conversation log with a system message
     let conversationLog = [{ role: 'system', content: 'You are a friendly chatbot.' }];
 
     try {
       await message.channel.sendTyping();
 
+      // Retrieve previous messages in the channel
       let prevMessages = await message.channel.messages.fetch({ limit: 15 });
       prevMessages = prevMessages.array().reverse();
 
+      // Add user messages to the conversation log
       prevMessages.forEach((msg) => {
         if (msg.author.id !== client.user.id && !msg.author.bot) {
           conversationLog.push({
@@ -93,11 +112,13 @@ client.on('messageCreate', async (message) => {
         }
       });
 
+      // Add the current message to the conversation log
       conversationLog.push({
         role: 'user',
         content: message.content,
       });
 
+      // Generate AI response using OpenAI API
       const result = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: conversationLog,
@@ -108,6 +129,7 @@ client.on('messageCreate', async (message) => {
         text = text.slice(0, MAX_MESSAGE_LENGTH) + '...';
       }
 
+      // Send the AI-generated response
       await message.reply(text);
     } catch (error) {
       console.error('Error:', error);
@@ -116,4 +138,7 @@ client.on('messageCreate', async (message) => {
 });
 
 const token = process.env.TOKEN || '';
-client
+
+// Log in to Discord using the bot token
+client.login(token);
+
