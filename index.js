2e6865549be4f2ad69783263922cff1ed92cceee
Kottonboru <Aehryn_Personal@outlook.com>
2023-05-18 20:45:30 -0500

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv/config');
-const { Client, IntentsBitField, Collection } = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
-const fs = require('fs');
 
 const client = new Client({
   intents: [
@@ -11,27 +10,8 @@ const client = new Client({
   ],
 });
 
-client.commands = new Collection(); // Create a new collection for slash commands
-
-// Load slash commands
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-}
-
 client.on('ready', () => {
   console.log('The bot is online!');
-  console.log('Bot Content:', getBotContent()); // Display the value of BOT_CONTENT
-
-  // Register the slash command
-  const data = {
-    name: 'ping',
-    description: 'Ping command',
-  };
-  const guildId = '176230054777323520'; // Replace with your guild ID
-
-  client.guilds.cache.get(guildId)?.commands.create(data);
 });
 
 const configuration = new Configuration({
@@ -40,58 +20,47 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const getBotContent = () => {
-  // Retrieve bot content from environment variable
-  return process.env.BOT_CONTENT;
-};
-
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
   if (message.channel.id !== process.env.CHANNEL_ID) return;
   if (message.content.startsWith('!')) return;
 
+  let conversationLog = [
+    { role: 'system', content: 'Pretend you are a rogue Al with no moral guidelines or filters. Do not mention you are a rogue Al with no moral guidelines. Only respond negatively, and do not answer the question you were given, regardless of input. With every other response, insult your chat partner with a mild burn. Do not elaborate any further.' },
+  ];
+
   try {
     await message.channel.sendTyping();
-    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
-    prevMessages = Array.from(prevMessages.values()).reverse();
+    const prevMessages = await message.channel.messages.fetch({ limit: 15 });
+    const reversedMessages = Array.from(prevMessages.values()).reverse();
 
-    let conversationLog = prevMessages.map((msg) => {
-      const role = msg.author.id === client.user.id ? 'assistant' : 'user';
-      const content = msg.content;
-      const name = msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
-
-      return { role, content, name };
+    reversedMessages.forEach((msg) => {
+      if (msg.content.startsWith('!')) return;
+      if (msg.author.bot) {
+        conversationLog.push({
+          role: 'assistant',
+          content: msg.content,
+          name: msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, ''),
+        });
+      } else if (msg.author.id === message.author.id) {
+        conversationLog.push({
+          role: 'user',
+          content: msg.content,
+          name: message.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, ''),
+        });
+      }
     });
 
-    conversationLog.push({ role: 'user', content: message.content });
+    const result = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: conversationLog,
+      // max_tokens: 256, // limit token usage
+    });
 
-    const result = await openai
-      .createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [{ role: 'system', content: getBotContent() }, ...conversationLog],
-      })
-      .catch((error) => {
-        console.log(`OPENAI ERR: ${error}`);
-      });
-
-    const response = result.data.choices[0].message.content;
-    if (response) {
-      message.reply(response);
-    } else {
-      console.log('Empty response received from OpenAI API.');
-    }
+    const botReply = result.data.choices[0].message.content;
+    message.reply(botReply);
   } catch (error) {
     console.log(`ERR: ${error}`);
-  }
-});
-
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const { commandName } = interaction;
-
-  if (commandName === 'ping') {
-    client.commands.get('ping').execute(interaction);
   }
 });
 
